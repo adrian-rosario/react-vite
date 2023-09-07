ec2287d445a77f69f2e81577df677d1d74558187
adrian-rosario <adrian.rosario@gmail.com>
2023-09-07 08:02:25 -0400

    layout propTypes

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,5 +1,10 @@
 import Header from "./header";
 import Footer from "./footer";
+import PropTypes from 'prop-types';
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
 
 export default function Layout({children}){
   return(
